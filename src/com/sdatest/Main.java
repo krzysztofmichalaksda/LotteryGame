62b1bdc2bafcdbc5f23adb71b9c00276fc07f3bd
unknown <krzysztof.michalak@setapp.pl>
2020-06-28 09:51:15 +0200

    Add random numbers

--- src/com/sdatest/Main.java
+++ src/com/sdatest/Main.java
@@ -4,16 +4,16 @@ import com.sdatest.engine.LotteryEngine;
 import com.sdatest.input.ConsoleInput;
 import com.sdatest.user.User;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         User user1 = ConsoleInput.getUserData();
         user1.welcome();
 
-        LotteryEngine lottery = new LotteryEngine(-3, 3);
-        for(int i=0; i<20; i++) {
-            System.out.print(lottery.getRandomNumber() + ", ");
-        }
-
+        LotteryEngine lottery = new LotteryEngine(1, 20, 4);
+        int[] randomNumbers = lottery.getMultipleInts();
+        System.out.println(Arrays.toString(randomNumbers));
     }
 }
--- src/com/sdatest/engine/LotteryEngine.java
+++ src/com/sdatest/engine/LotteryEngine.java
@@ -1,14 +1,32 @@
 package com.sdatest.engine;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class LotteryEngine {
     int start;
     int end;
+    int count;
 
-    public LotteryEngine(int start, int end) {
+    public LotteryEngine(int start, int end, int count) {
         this.start = start;
         this.end = end;
+        this.count = count;
+    }
+
+    public int[] getMultipleInts()
+    {
+        int[] randomNumbers = new int[count];
+        for (int i = 0; i < randomNumbers.length; i++)
+        {
+            int randomNumber;
+
+            randomNumber = getRandomNumber();
+
+            randomNumbers[i] = randomNumber;
+        }
+
+        return randomNumbers;
     }
 
     public int getRandomNumber()
